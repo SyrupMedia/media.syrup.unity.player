ea993bde132ed9f1be30c5ec65ec7a31a0a78c0d
MolassesLover <arthur@garsi.de>
2021-12-18 16:12:14 +0100

    Updated to Unity 2020.3.25f1 and added gravity --* The project version is now set to Unity 2020.3.25f1, an LTS release.
    --* A bug involving the `isWalking` boolean in the player's animation
    controller has been fixed. The boolean will now be set to true if the
    player is walking,and/or false if the player is idle.
    
    --* Gravity relying on the [Character Controller](https://docs.unity3d.com/ScriptReference/CharacterController.html) component has been implemented. It uses the `CheckSphere()` function to determine whether the player is on the ground or not.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,13 +9,23 @@ namespace SyrupPlayer
     public class Player : MonoBehaviour
     {
         #region Variables
-    
+
+        [Header("Physics")]
+        public bool applyGravity;
+        public float gravity = -9.81f;
+        private Vector3 verticalVelocity;
+        private bool isGrounded;   
+        public Transform groundCheck;
+        public float groundDistance = 0.5f;
+        public LayerMask groundLayer;
+
+
         [Header("Graphics")]
         public Animator animator;
 
-        [Header("Movement")]	
+        [Header("Movement")]
         public CharacterController controller;
-    
+
         [Header("Camera")]
         public Transform mainCamera;
         private GameObject[] cameras;
@@ -23,7 +33,7 @@ namespace SyrupPlayer
         [Header("Controls")]
         [Tooltip("Locks the player's mouse.")]
         public bool lockedMouse;
-    
+
         [Header("Locomotion")]
         [Tooltip("Determines the player's movement speed.")]
         [Range(1f, 10f)]
@@ -33,22 +43,22 @@ namespace SyrupPlayer
         [Min(0.1f)]
         public float turnSmoothing = 0.1f;
         private float turnVelocity;
-    
+
         #endregion
-    
+
         #region Unity Methods
-    
+
         // Awake is called when the script instance is being loaded.
         private void Awake()
         {
             cameras = GameObject.FindGameObjectsWithTag("MainCamera");
             mainCamera = cameras[0].GetComponent<Transform>();
-        } 
-    
+        }
+
         // Start() is called before the first frame update.
         private void Start()
         {
-            if(lockedMouse)
+            if (lockedMouse)
             {
                 Cursor.lockState = CursorLockMode.Locked;
             }
@@ -61,37 +71,54 @@ namespace SyrupPlayer
         // Update() is called once per frame.
         private void Update()
         {
-            if(Input.GetButtonDown("Cancel"))
+            if (Input.GetButtonDown("Cancel"))
             {
                 Cursor.lockState = CursorLockMode.None;
                 lockedMouse = false;
             }
-        
+
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
-            
+
             animator.SetFloat("horizontal", horizontal);
             animator.SetFloat("vertical", vertical);
-            
+
             Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-            
-            if(direction.magnitude >= 0.1f)
-            {      
+
+            if (direction.magnitude >= 0.1f)
+            {
+                animator.SetBool("isWalking", true);
+
                 float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnSmoothing);
-                
+
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
-                
+
                 Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
                 controller.Move(moveDirection.normalized * movementSpeed * Time.deltaTime);
-                
             }
             else
-            {   
+            {
                 animator.SetBool("isWalking", false);
             }
-        }   
-    
+
+            if (applyGravity == true)
+            {
+                // This is the same approach featured in Brackeys' FPS Controller.
+                // https://youtu.be/_QajrabyTJc?t=1132
+                isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+
+                if (isGrounded && verticalVelocity.y < 0)
+                {
+                    verticalVelocity.y = -2.5f;
+                }
+
+                verticalVelocity.y += gravity * Time.deltaTime;
+
+                controller.Move(verticalVelocity * Time.deltaTime); 
+            }
+        }
+
         #endregion
     }
 }
